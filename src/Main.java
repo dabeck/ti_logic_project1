e9364842cce671d282b706dad838c44d3e5fb63d
muellerp <mw@live.de>
2013-11-16 15:05:57 +0100

    JFlex Lib hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 		try {
-			parser p = new parser(new Scanner(new StringReader("-A & B")));
-		    //parser p = new parser(new Scanner(new StringReader("-A & (B | -(C <-> HalloWelt) | (-B -> A -> -C))")));
+		    parser p = new parser(new Scanner(new StringReader("-A & (B | -(C <-> HalloWelt) | (-B -> A -> -C))")));
 		    Formula f = (Formula) p.parse().value;
-		    System.out.println("TEST: " + p.parse().value.toString());
 		}
 		catch (Exception e) {
 		}
