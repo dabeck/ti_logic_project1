241cf4abeb6dfde106d02a4a0281c5fd3977875a
daniel <baniel.decker@gmail.com>
2013-11-18 12:34:49 +0100

    refactored biimplication to equivalence

---
+++ src/Equivalence.java
@@ -0,0 +1,17 @@
+public class Equivalence extends Formula {
+
+    private Formula left_arg, right_arg;
+
+    public Equivalence(Formula f, Formula g) {
+    	left_arg = f;
+    	right_arg = g;
+    }
+
+    public Formula getLeftArg() {
+    	return left_arg;
+    }
+
+    public Formula getRightArg() {
+    	return right_arg;
+    }
+}
