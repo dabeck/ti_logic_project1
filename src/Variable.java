39c037c5f97b52784c59badcf47aa49e95a54a54
daniel <baniel.decker@gmail.com>
2013-11-18 12:35:26 +0100

    added bool value to variable

--- src/Variable.java
+++ src/Variable.java
@@ -1,11 +1,21 @@
 public class Variable extends Formula {
 
 	private String name;
+	private Boolean value;
  
     public Variable(String x) {
     	name = x;
+		value = true;
     }
 	
+	public void setValue(Boolean val) {
+		value = val;
+	}
+	
+	public Boolean getValue() {
+		return value;
+	}
+
 	public String getVar() {
 		return name;
 	}
