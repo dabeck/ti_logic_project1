ba6d023416fba843148ff1779cc37921ea0c518d
daniel <baniel.decker@gmail.com>
2013-11-21 13:38:01 +0100

    refactored parsetree to its own class

--- src/Main.java
+++ src/Main.java
@@ -32,49 +32,11 @@ public class Main {
 			Formula g = Convert.toCnf( f );
 			System.out.println( "-> KNF:\t" + g );
 			
-			parseTree(f);
+			Satisfiability.parseTree(f);
 		
 		}
 		catch (Exception e) {
 			System.out.println( "Fehler:\t" + e.toString() );
 		}
-    }
-	
-	public static void parseTree(Formula x) {
-		System.out.print("(");
-		if (x instanceof Negation) {
-			System.out.print("NOT ");
-		}
-
-		if(x instanceof Variable){
-			Variable s = (Variable)x;
-			System.out.print(s.getVar());
-		}
-		
-		if (x.getLeftArg() != null) {
-			parseTree(x.getLeftArg());
-		}
-		
-		if (x instanceof Conjunction) {
-			System.out.print(" AND ");
-		}
-		
-		if (x instanceof Disjunction) {
-			System.out.print(" OR ");
-		}
-		
-		if (x instanceof Implication) {
-			System.out.print(" IMPLIES ");
-		}
-		
-		if (x instanceof Equivalence) {
-			System.out.print(" EQUIVALENT ");
-		}
-
-		if (x.getRightArg() != null) {
-			parseTree(x.getRightArg());
-		}
-		System.out.print(")");
-
     }
 }
