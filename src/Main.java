3a3fdf6da8ebc18e3024aa1e6eb6fdbf05759eef
daniel <baniel.decker@gmail.com>
2013-11-22 09:54:15 +0100

    Added calculation timer

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import formula;
 */
 
 import java.io.StringReader;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -18,13 +19,12 @@ public class Main {
 			// "A | B" // KNF korrekt: (A | B)
 			                // "-(A & B)" // KNF korrekt: (-A | -B)
 			// "-(A | B)" // KNF korrekt: (-A & -B)
-			                // "A -> B" // KNF korrekt: (-A | B)
-			                // "-A & (B | -(C <-> D) | (-B -> A -> -C))"
-			                // "A <-> B" // KNF korrekt: ((A | -B) & (-A
-							// | B))
-			                // "(-A & B) & -(C <-> D) | (-B -> A -> -C)"
-			                        "((((((((((X0<->X1)<->X2)<->X3)<->X4)<->X5)<->X6)<->X7)<->X8)<->X9)<->X10)&(X10<->X8)&(X9<->X7)&(X8<->X6)&(X7<->X5)&(X6<->X4)&(X5<->X3)&(X4<->X2)&(X3<->X1)&(X2<->X0)&-(X1<->X0)"
-					)));
+			                        // "A -> B" // KNF korrekt: (-A | B)
+			                        // "-A & (B | -(C <-> D) | (-B -> A -> -C))"
+			                        // "A <-> B" // KNF korrekt: ((A |
+									// -B) & (-A | B))
+			                        // "(-A & B) & -(C <-> D) | (-B -> A -> -C)"
+			                        "((((((((((X0<->X1)<->X2)<->X3)<->X4)<->X5)<->X6)<->X7)<->X8)<->X9)<->X10)&(X10<->X8)&(X9<->X7)&(X8<->X6)&(X7<->X5)&(X6<->X4)&(X5<->X3)&(X4<->X2)&(X3<->X1)&(X2<->X0)&-(X1<->X0)")));
 
 			// parse die Formel
 			Formula f = (Formula) p.parse().value;
@@ -33,14 +33,20 @@ public class Main {
 			
 			// Konvertiere nach KNF
 			// ToDo: noch nicht komplett
-			Formula g = Convert.toCnf( f );
+			// Formula g = Convert.toCnf( f );
 			// System.out.println( "-> KNF:\t" + g );
 			
+			long startTime = System.nanoTime();
 			if (Satisfiability.check(f)) {
 				System.out.println("Die gegebene Formel ist erfüllbar!");
 			} else {
 				System.out.println("Die gegebene Formel ist NICHT erfüllbar!");
 			}
+			long estimatedTime = System.nanoTime() - startTime;
+
+			System.out.println("\n\nCalculation time: "
+			        + TimeUnit.MILLISECONDS.convert(estimatedTime,
+			                TimeUnit.NANOSECONDS) + "ms");
 		
 		}
 		catch (Exception e) {
